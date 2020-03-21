c88dff5154f6ac0eed51455991ee1fdc88c69c43
Tahmina Akter <tahmina.akter9519@gmail.com>
2020-03-21 22:06:37 +0600

    Users now can see recycler view items in details on another activity.Hyper link capability added.

--- app/src/main/java/com/example/lostandfound/ItemDescription.java
+++ app/src/main/java/com/example/lostandfound/ItemDescription.java
@@ -3,36 +3,63 @@ package com.example.lostandfound;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 public class ItemDescription extends AppCompatActivity {
-    TextView itemName,emailAdd,contactNo;
+    TextView itemName,emailAdd,contactNo,dateNo;
+    ImageView imageView;
+    String name,url,number,email,date;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_description);
-        /*
-                intent.putExtra("name",up.getName());
-                intent.putExtra("url",up.getImageUrl());
-                intent.putExtra("number",up.getNumber());
-                intent.putExtra("email",up.getEmail());
-                intent.putExtra("date",up.getDate());
-         */
         Intent intent = getIntent();
-        String name = intent.getStringExtra("name");
-        String url = intent.getStringExtra("url");
-        String number = intent.getStringExtra("number");
-        String email = intent.getStringExtra("email");
-        String date = intent.getStringExtra("date");
+        name = intent.getStringExtra("name");
+        url = intent.getStringExtra("url");
+        number = intent.getStringExtra("number");
+        email = intent.getStringExtra("email");
+        date = intent.getStringExtra("date");
 
         itemName = findViewById(R.id.item_name_ind);
         emailAdd = findViewById(R.id.email_ind);
         contactNo = findViewById(R.id.number_ind);
+        dateNo = findViewById(R.id.date_ind);
+        imageView = findViewById(R.id.image_view_upload);
 
         itemName.setText(name);
         emailAdd.setText(email);
         contactNo.setText(number);
+        dateNo.setText(date);
+        Picasso.get()
+                .load(url)
+                .placeholder(R.mipmap.ic_launcher)
+                .fit()
+                .centerCrop()
+                .into(imageView);
 
+    }
+
+    public void openDialer(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:"+number));
+        startActivity(intent);
+    }
+
+    public void openGmail(View view){
+        Intent intent=new Intent(Intent.ACTION_SEND);
+        String[] recipients={email};
+        intent.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intent.putExtra(Intent.EXTRA_SUBJECT,"I think this lost item is mine!");
+        intent.putExtra(Intent.EXTRA_TEXT,"Body of the content here...");
+        intent.putExtra(Intent.EXTRA_CC,"mailcc@gmail.com");
+        intent.setType("text/html");
+        intent.setPackage("com.google.android.gm");
+        startActivity(Intent.createChooser(intent, "Send mail"));
     }
 }
